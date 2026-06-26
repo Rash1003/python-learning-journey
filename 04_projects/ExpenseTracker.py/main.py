f54c06410a93f7687fe5a48b6a51c38ec672f448
Rashmi Chaube <154296175+Rash1003@users.noreply.github.com>
2026-06-26 11:59:23 +0530

    Added Add expense feature

--- 04_projects/ExpenseTracker.py/main.py
+++ 04_projects/ExpenseTracker.py/main.py
@@ -6,3 +6,26 @@ print("4. Highest Expense")
 print("5. Search Category")
 print("6. Exit")
 
+expenses = []
+
+while True:
+    category = input("Enter category: ")
+    amount = float(input("Enter amount: ₹"))
+
+    expense = {
+        "category": category,
+        "amount": amount
+    }
+
+    expenses.append(expense)
+
+    choice = input("Add another expense? (y/n): ")
+
+    if choice.lower() == "n":
+        break
+
+print("\nExpenses:")
+for expense in expenses:
+    print(f"{expense['category']} : ₹{expense['amount']}")
+
+    
